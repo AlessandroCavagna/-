91ac75ab0f2b417d2db0f7e23b81725df1b7b0fc
Алессандро Каванья <alescavagna2008@gmail.com>
2023-12-01 20:23:27 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 import sys
+import io
 import random
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-
+template = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>483</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="CircleButton">
+    <property name="geometry">
+     <rect>
+      <x>170</x>
+      <y>490</y>
+      <width>131</width>
+      <height>31</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Нажми меня</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>483</width>
+     <height>21</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>"""
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
 
         self.do_paint = False
 
@@ -29,10 +75,14 @@ class MyWidget(QMainWindow):
         self.update()
 
     def draw_flag(self, qp):
+        r = random.randint(0, 256)
+        g = random.randint(0, 256)
+        b = random.randint(0, 256)
+
         D = random.randint(20, 121)
         x = random.randint(int(D / 2), 480 - int(D / 2))
         y = random.randint(int(D / 2), 450 - int(D / 2))
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(r, g, b))
         qp.drawEllipse(QPoint(x, y), int(D / 2), int(D / 2))
 
 
